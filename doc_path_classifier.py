fb7e2b9f8b3d1e6dc2483de8f06d478dc421c634
gitLSW <61458756+gitLSW@users.noreply.github.com>
2025-04-14 00:17:54 +0200

    Update doc_path_classifier.py

--- doc_path_classifier.py
+++ doc_path_classifier.py
@@ -4,7 +4,7 @@ class DocPathClassifier:
     def __init__(self, company_id):
         self.company_id = company_id
         self.vectorizer = joblib.load(f'./{company_id}/classifier/vectorizer.pkl')
-        self.classifier = joblib.load('./{company_id}/classifier/classifier.pkl')
+        self.classifier = joblib.load(f'./{company_id}/classifier/classifier.pkl')
 
     def classify_doc(self, content):
         # Vectorize the input and predict.
