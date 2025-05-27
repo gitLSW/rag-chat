105605b0e0143c02bb4e83fe63731f19c667b720
L Weckerle <overflow.operator@gmail.com>
2025-05-27 13:58:57 +0200

    Improvement

--- server/middleware/auth/api_access_middlware.py
+++ server/middleware/auth/api_access_middlware.py
@@ -1,5 +1,8 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request, HTTPException
+from utils import get_env_var
+
+IS_PROD_ENV = get_env_var('IS_PROD_ENV')
 
 
 class APIAccessMiddleware(BaseHTTPMiddleware):
@@ -18,7 +21,7 @@ class APIAccessMiddleware(BaseHTTPMiddleware):
 
         # Verify IP address
         client_ip = request.client.host
-        if client_ip not in self.allowed_ips:
+        if client_ip not in self.allowed_ips and not IS_PROD_ENV:
             raise HTTPException(403, "Forbidden - IP address not allowed")
 
         # Verify API key
