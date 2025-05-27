50cdb4a792f503104204c72042c3cf991fce2b52
L Weckerle <overflow.operator@gmail.com>
2025-05-27 13:51:37 +0200

    fix

--- server/api_server.py
+++ server/api_server.py
@@ -202,5 +202,5 @@ async def search_docs(req: SemanticSearchReq):
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=PORT)
     logger.info("Started Server...")
+    uvicorn.run(app, host='0.0.0.0', port=PORT)
